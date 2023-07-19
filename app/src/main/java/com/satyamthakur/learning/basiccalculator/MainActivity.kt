a95838ae8c59cd083b8d8f57d5ba3a5f15e07d94
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-19 12:34:48 +0530

    Implementing onEqual function and onDigitClick

--- app/src/main/java/com/satyamthakur/learning/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/basiccalculator/MainActivity.kt
@@ -2,8 +2,15 @@ package com.satyamthakur.learning.basiccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.core.view.isVisible
 import com.satyamthakur.learning.basiccalculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.ArithmeticException
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +27,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bind.root)
+    }
+
+    fun onDigitClick(view: View) {
+        if (stateError) {
+            bind.inputTv.text = (view as Button).text
+            stateError = false
+        }
+        else {
+            bind.inputTv.append((view as Button).text)
+        }
+
+        lastNumeric = true
+        onEqual()
+    }
+
+    private fun onEqual() {
+        if (lastNumeric && !stateError) {
+            val txt = bind.inputTv.text.toString()
+            expression = ExpressionBuilder(txt).build()
+
+            try {
+                val result = expression.evaluate()
+                bind.outputTv.isVisible = true
+                bind.outputTv.text = result.toString()
+            }
+            catch (e: ArithmeticException) {
+                Log.e("MYAPP", "Evaluation error $e")
+                bind.outputTv.text = "Error!"
+                stateError = true
+                lastNumeric = false
+            }
+        }
     }
 
     override fun onDestroy() {
