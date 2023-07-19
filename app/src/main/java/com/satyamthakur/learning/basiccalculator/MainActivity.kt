17e319c7f6f748a4e06c2dab3e8ccd08512a731d
Satyam Thakur <sat.thakur20@gmail.com>
2023-07-19 12:19:50 +0530

    Using viewbinding in mainactivity and declaring some var

--- app/src/main/java/com/satyamthakur/learning/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/basiccalculator/MainActivity.kt
@@ -2,10 +2,28 @@ package com.satyamthakur.learning.basiccalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.satyamthakur.learning.basiccalculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.Expression
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    private val bind get() = binding!!
+
+    var lastNumeric = false
+    var stateError = false
+    var lastDot = false
+
+    private lateinit var expression: Expression
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
